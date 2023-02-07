87ed79b47a6d73eabc9b23cc0bbff2362ab58380
Dmitry <pain2284@gmail.com>
2023-02-07 20:14:58 +0500

    video section
    video section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { BsFillPLayFill,BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
+const Intro = () => {
+  const [ playVideo, setPlayVideo ] = useState(false)
+  const vidRef = React.useRef()
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo)=> !prevPlayVideo)
+
+    if(playVideo){
+      vidRef.current.pause()
+    } else {
+      vidRef.current.play()
+    }
+  }
+  return(
+  <div className='app__video'>
+    <video
+    src={meal}
+    ref={vidRef}
+    type='video/mp4'
+    Loop
+    controls={false}
+    muted
+     />
+     <div className='app__video-overlay flex__center'>
+      <div 
+      className='app__video-overlay_circle flex__center'
+      onClick={handleVideo}
+      >
+
+      </div>
+     </div>
   </div>
-);
+)};
 
 export default Intro;
